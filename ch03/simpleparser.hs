437c1fd6de8e29e05f9d0ad9d79a5ad9c2b4dbfa
tsuy <spaceminer@protonmail.com>
2021-06-21 15:00:10 -0400

    change eval back

--- ch03/simpleparser.hs
+++ ch03/simpleparser.hs
@@ -172,12 +172,12 @@ unwordsList = unwords . map showVal
 instance Show LispVal where show = showVal
 
 eval :: LispVal -> LispVal
--- eval val@(String _) = val
--- eval val@(Number _) = val
--- eval val@(Bool _) = val
+eval val@(String _) = val
+eval val@(Number _) = val
+eval val@(Bool _) = val
 eval (List [Atom "quote", val]) = val
 eval (List (Atom func : args)) = apply func $ map eval args
-eval val = val
+-- eval val = val
 
 apply :: String -> [LispVal] -> LispVal
 apply func args = maybe (Bool False) ($ args) $ lookup func primitives
